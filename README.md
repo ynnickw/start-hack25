62fb4789de90fba855eae11afc4bae1abf70bd73
yannick.westermann <yannick.westermann@check24.de>
2025-03-19 23:19:13 +0100

    react chat app basic template

--- README.md
+++
@@ -1,1 +0,0 @@
-# start-hack25
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-2xl font-bold text-gray-900">
+            syngenta prodcut recommendation
+          </h1>
+        </div>
+      </header>
+      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="bg-white rounded-lg shadow h-[600px]">
+          <Chat />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App; 
